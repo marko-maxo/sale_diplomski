bd862013827393cfcde68f8a4a6f3d65b528abc1
Luka Ilić <luka.ilic@rbt.rs>
2024-09-27 17:44:04 +0200

    Idemo gas

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,14 @@
-from .account import Account, AccountBalance, PreStageAccountBalance, PreStageAccount
-from .currency import Currency, PreStageCurrency
-from .branch import Branch, PreStageBranch
-from .customer import Customer, PreStageCustomer
-from .transaction import Transaction, PreStageTransaction
+from .account import (
+    Account,
+    AccountBalance,
+    PreStageAccountBalance,
+    PreStageAccount,
+    StageAccount,
+    StageAccountBalance,
+    DWHAccount,
+    DWHAccountBalance,
+)
+from .currency import Currency, PreStageCurrency, StageCurrency, DWHCurrency
+from .branch import Branch, PreStageBranch, StageBranch, DWHBranch
+from .customer import Customer, PreStageCustomer, StageCustomer, DWHCustomer
+from .transaction import Transaction, PreStageTransaction, StageTransaction, DWHTransaction
